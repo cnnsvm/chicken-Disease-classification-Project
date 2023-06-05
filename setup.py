80afb88c7861642141c4c4c138e8087fcfafc9d3
cnnsvm <128366940+cnnsvm@users.noreply.github.com>
2023-06-05 11:34:27 +0530

    Data ingestion completed

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ __version__ = "0.0.0"
 REPO_NAME = "Chicken-Disease-Classification--Project"
 AUTHOR_USER_NAME = "entbappy"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "cnnsvm3@gmail.com"
 
 
 setuptools.setup(
